c932461b2d334f12155ef04953a5425340b42613
Galimede <mat.degand@outlook.fr>
2021-01-05 15:40:14 +0100

    :wrench: try with $ for env maybe

--- conduit/settings.py
+++ conduit/settings.py
@@ -25,9 +25,9 @@ class Config(object):
         'http://localhost:4200',
         'http://0.0.0.0:4000',
         'http://localhost:4000',
-        'http://0.0.0.0:' + os.environ.get('PORT', 8080),
-        'https://0.0.0.0:' + os.environ.get('PORT', 8080),
-        os.environ.get('BACK_PROD_URL'),
+        'http://0.0.0.0:' + os.environ.get('$PORT', 8080),
+        'https://0.0.0.0:' + os.environ.get('$PORT', 8080),
+        os.environ.get('$BACK_PROD_URL'),
         os.environ.get('FRONT_PROD_URL'),
     ]
     JWT_HEADER_TYPE = 'Token'
