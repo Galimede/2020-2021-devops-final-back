2c5f76b3ed0856efc3b04e14ba4161a3612926d7
Galimede <mat.degand@outlook.fr>
2021-01-05 15:36:26 +0100

    :wrench: add https to generic

--- conduit/settings.py
+++ conduit/settings.py
@@ -26,6 +26,7 @@ class Config(object):
         'http://0.0.0.0:4000',
         'http://localhost:4000',
         'http://0.0.0.0:' + os.environ.get('PORT', 8080),
+        'https://0.0.0.0:' + os.environ.get('PORT', 8080),
         os.environ.get('BACK_PROD_URL'),
         os.environ.get('FRONT_PROD_URL'),
     ]
