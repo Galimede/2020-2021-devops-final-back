44a281794563d3d880e04c3caf35d79ec318cec9
Galimede <mat.degand@outlook.fr>
2021-01-05 15:48:09 +0100

    :wrench: Now works in a generic way->get rid of old one

--- conduit/settings.py
+++ conduit/settings.py
@@ -27,7 +27,6 @@ class Config(object):
         'http://localhost:4000',
         'http://0.0.0.0:' + os.environ.get('$PORT', '8080'),
         'https://0.0.0.0:' + os.environ.get('$PORT', '8080'),
-        os.environ.get('$BACK_PROD_URL', ''),
         os.environ.get('FRONT_PROD_URL', ''),
     ]
     JWT_HEADER_TYPE = 'Token'
