0ee7136f444045ef6aff05a5dd40caff6393e88d
Galimede <mat.degand@outlook.fr>
2021-01-05 13:02:30 +0100

    :wrench: Change env var name

--- conduit/settings.py
+++ conduit/settings.py
@@ -25,7 +25,7 @@ class Config(object):
         'http://localhost:4200',
         'http://0.0.0.0:4000',
         'http://localhost:4000',
-        os.environ.get('PROD_URL')
+        os.environ.get('FRONT_PROD_URL')
     ]
     JWT_HEADER_TYPE = 'Token'
 
